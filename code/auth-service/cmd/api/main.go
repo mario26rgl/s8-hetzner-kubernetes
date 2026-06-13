7e0192228d4dc8d9e726a0f691edc9cdef87bb70
Mario Constantin <mario.constantin@vodafone.com>
2026-06-13 19:05:39 +0300

    static fixes

--- code/auth-service/cmd/api/main.go
+++ code/auth-service/cmd/api/main.go
@@ -9,9 +9,7 @@ import (
 	"os"
 	"time"
 
-	_ "github.com/jackc/pgconn"
-	_ "github.com/jackc/pgx/v4"
-	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 const webPort = "80"
--- code/logger-service/data/models.go
+++ code/logger-service/data/models.go
@@ -57,7 +57,7 @@ func (l *LogEntry) All() ([]*LogEntry, error) {
 	collection := client.Database("logs").Collection("logs")
 
 	opts := options.Find()
-	opts.SetSort(bson.D{{"created_at", -1}})
+	opts.SetSort(bson.D{{Key: "created_at", Value: -1}})
 
 	cursor, err := collection.Find(ctx, bson.D{}, opts)
 	if err != nil {
@@ -135,10 +135,10 @@ func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
 		ctx,
 		bson.M{"_id": docID},
 		bson.D{
-			{"$set", bson.D{
-				{"name", l.Name},
-				{"data", l.Data},
-				{"updated_at", time.Now()},
+			{Key: "$set", Value: bson.D{
+				{Key: "name", Value: l.Name},
+				{Key: "data", Value: l.Data},
+				{Key: "updated_at", Value: time.Now()},
 			}},
 		},
 	)
